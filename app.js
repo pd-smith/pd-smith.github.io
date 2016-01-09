215bc208b5b3f12d600fa8b196168e02607d9895
Patrick Smith <Patty@Patricks-MacBook-Pro.local>
2016-01-09 02:12:28 -0500

    Under Construction page with some organization

---
+++ app.js
@@ -0,0 +1,3 @@
+(function(){
+    var app = angular.module("personal-site", ["site-navigation"]);
+})();
---
+++ src/js/navigation.js
@@ -0,0 +1,10 @@
+(function(){
+    var app = angular.module("site-navigation",[]);
+    
+    app.directive("navigationBar", function(){
+       return {
+         restrict : 'E',
+         templateUrl : '/src/pages/navigation-bar.html'
+       };
+    });
+})();
