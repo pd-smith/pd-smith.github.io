4903ec76dbfd67c3c1ea7092dfa4bd5315854297
Patrick Smith <Patty@dhcp020-209-103.wireless.buffalo.edu>
2016-01-25 23:57:14 -0500

    Put basic description and contact info. Needs cleaning up

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 (function(){
     var app = angular.module("personal-site", ['site-navigation','projects']);
+    
+    app.directive("navigation", [function(){
+       return{
+           restrict : 'E',
+           templateUrl : '/src/pages/index.html',
+           controller : function(){
+               this.viewIndex = 1;
+           },
+           controllerAs : 'viewpage'
+       }; 
+    }]);
 })();
