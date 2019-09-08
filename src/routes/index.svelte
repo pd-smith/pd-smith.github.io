c11a040a1687230399613e167f458ebf454e9337
pd-smith <psmithdb@gmail.com>
2019-09-07 23:58:08 -0400

    fun little animation thing"

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -8,6 +8,40 @@
 		animation-iteration-count: infinite;
 		animation-fill-mode: backwards;
 		animation-direction: alternate;	}
+	.animation-container {
+		position: absolute;
+		width: 100%;
+		max-width: 20rem;
+		height: 15rem;
+		
+	}
+	.animation-container :nth-child(even) {
+		background-color: #218796;
+	}
+	.delay-1 {
+		animation-delay: -3s;
+	}
+	.delay-2 {
+		animation-delay: -6s;
+	}
+	.delay-3 {
+		animation-delay: -9s;
+	}
+	.boing {
+		position: relative;
+		height: 50px;
+		width: 50px;
+		animation-name: horizontal-boing;
+		border-radius: 50%;
+		background: rgb(247, 184, 207);
+		animation-duration: 12s;
+		animation-iteration-count: infinite;
+		opacity: .5;
+	}
+	.align-center {
+		display: flex;
+		justify-content: center;
+	}
 	.title h2 {
 		font-size: 30px;
 		margin: 5px 0;
@@ -26,6 +60,13 @@
 		from {text-shadow: 5px 2px 1px #442986}
 		to {text-shadow: 4px 2px 3px rgb(160, 12, 245)}
 	}
+	@keyframes horizontal-boing {
+		0% { top: 25%; left: 25%; }
+		25% { top: 25%; left: 75%; }
+		50% { top: 75%; left: 75%; }
+		75% { top: 75%; left: 25%; }
+		100% { top: 25%; left: 25%; }
+	}
 	
 </style>
 
@@ -43,4 +84,11 @@
 		</h2>
 	</main>
 </section>
-
+<div class="align-center">
+	<div class="animation-container">
+		<div class="boing"></div>
+		<div class="boing delay-1"></div>
+		<div class="boing delay-2"></div>
+		<div class="boing delay-3"></div>
+	</div>
+</div>
